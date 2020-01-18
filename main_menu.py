7040b25800dd3e05ddf080a50dabc022484fc792
sabonlise <46754745+sabonlise@users.noreply.github.com>
2020-01-18 12:02:06 +0200

    Разделение duo на duo_classic и duo_time

--- main_menu.py
+++ main_menu.py
@@ -49,9 +49,12 @@ def main_menu(window):
 
         button_solo.draw(window)
         button_duo.draw(window)
+        button_duo_time.draw(window)
+
         draw_text_middle(window, 'Выберите режим игры',
                          60, (255, 255, 255))
         pygame.display.update()
+
         for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
             if event.type == pygame.QUIT:
@@ -64,24 +67,39 @@ def main_menu(window):
                     subprocess.Popen(['solo.py'], shell=True, creationflags=subprocess.SW_HIDE)
 
                 elif button_duo.is_clicked(pos):
-                    subprocess.Popen(['duo.py'], shell=True, creationflags=subprocess.SW_HIDE)
+                    subprocess.Popen(['duo_classic.py'], shell=True, creationflags=subprocess.SW_HIDE)
+
+                elif button_duo_time.is_clicked(pos):
+                    subprocess.Popen(['duo_time.py'], shell=True, creationflags=subprocess.SW_HIDE)
 
             if event.type == pygame.MOUSEMOTION:
                 if button_solo.is_clicked(pos):
                     button_solo.color = (170, 255, 0)
-                elif button_duo.is_clicked(pos):
-                    button_duo.color = (0, 229, 238)
+                else:
+                    button_solo.color = (0, 255, 0)
+                    if button_duo.is_clicked(pos):
+                        button_duo.color = (0, 229, 238)
+                    else:
+                        button_duo.color = (0, 0, 255)
+                        if button_duo_time.is_clicked(pos):
+                            button_duo_time.color = (0, 229, 238)
+                        else:
+                            button_duo_time.color = (0, 0, 255)
 
     pygame.display.quit()
 
 
-button_solo = Button((0, 255, 0), 150, 380, 200, 50, 'Соло игра')
-button_duo = Button((0, 0, 255), 450, 380, 200, 50, 'Дуо игра')
+button_solo = Button((0, 255, 0), 290, 315, 200, 50, 'Solo')
+button_duo = Button((0, 0, 255), 425, 400, 200, 50, 'Duo classic')
+button_duo_time = Button((0, 0, 255), 170, 400, 200, 50, 'Duo time')
 pygame.font.init()
+
 s_width, s_height = 800, 700
 play_width, play_height = 300, 600
+
 top_left_x = (s_width - play_width) // 2  # верхняя левая координата X
 top_left_y = s_height - play_height       # верхняя левая координата Y
+
 window = pygame.display.set_mode((s_width, s_height))
 pygame.display.set_caption('Тетрис')
 main_menu(window)
